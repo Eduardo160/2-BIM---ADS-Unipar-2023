e0cffc55cb1ff32999dc63cc25846265d6e32118
Eduardo160 <130612696+Eduardo160@users.noreply.github.com>
2023-10-01 22:22:35 -0300

    Trabalho 1° Eng. Software
    RA: 00247204 - Eduardo Kauan
    RA: 0025354 - Gabriel Simão
    RA: 00256045 - Lucas Piva
    RA: 00251536 - Vinicius Pasqualini

---
+++ TRAB ENG. DE SOFTWRE/Links P ATV/Java Script/scriptlinkpatv.js
@@ -0,0 +1,29 @@
+let btAtv1 = document.querySelector("#btAtv1");
+
+btAtv1.onclick = function () {
+  window.open("../MetodologiaCrystal/indexcrystal.html");
+};
+
+let btAtv2 = document.querySelector("#btAtv2");
+
+btAtv2.onclick = function () {
+  window.open("../MetodologiaFDD/indexfdd.html");
+};
+
+let btAtv3 = document.querySelector("#btAtv3");
+
+btAtv3.onclick = function () {
+  window.open("../MetodologiaKANBAN/indexkanban.html");
+};
+
+let btAtv4 = document.querySelector("#btAtv4");
+
+btAtv4.onclick = function () {
+  window.open("../MetodologiaMSF/indexmsf.html");
+};
+
+let btAtv5 = document.querySelector("#btAtv5");
+
+btAtv5.onclick = function () {
+  window.open("../MetodologiaXP/indexxp.html");
+};
